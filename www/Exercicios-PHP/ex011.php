a4b9bf7e89e46aa1235d8e16de6ea7a87425b504
Matheus <99matheussan@gmail.com>
2022-05-27 14:30:48 -0300

    update

--- www/Exercicios-PHP/ex011.php
+++ www/Exercicios-PHP/ex011.php
@@ -3,8 +3,8 @@
 
 <body>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Numero1: <input type="number" name="fnumero1">
-        Numero2: <input type="number" name="fnumero2">
+        Numero1: <input type="number" name="fnumero1" value=<?php echo $var=52?>>
+        Numero2: <input type="number" name="fnumero2" value=<?php echo $var =106?>>
         <input type="submit">
     </form>
 </body>
@@ -14,10 +14,9 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $numero = $_POST['fnumero1'];
     $numero2 = $_POST['fnumero2'];
-    $var1 = 52;
-    $var2 = 106;
+
     
-    echo 'O primeiro número digitado foi: ' . $numero . '<br> O segundo número foi: ' . $numero2 . '<br> A soma dos dois número é igual a: ' . $numero + $numero2 . '52 + 106 = ' . $var1 + $var2;
+    echo 'O primeiro número digitado foi: ' . $numero . '<br> O segundo número foi: ' . $numero2 . '<br> A soma dos dois número é igual a: ' . $numero + $numero2;
 }
 ?>
 <?php
