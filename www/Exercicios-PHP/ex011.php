a9531149e8b4fd3fe70d2c49e22102b10e5638d0
Matheus <99matheussan@gmail.com>
2022-05-27 17:22:27 -0300

    update

--- www/Exercicios-PHP/ex011.php
+++ www/Exercicios-PHP/ex011.php
@@ -1,17 +1,18 @@
 <!-- Solicite os valores de num1 e num2. Some os valores das variaveis num1 e num2: sendo num1=52 e num2 = 106. por fim exiba o resultado. -->
 <html>
 <?php
-    $n1 = 52; $n2 = 106;
+ 
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         if(empty($_POST['numero1']) || empty($_POST['numero2'])) {
             $n1 = 52;
             $n2 = 106;
-            echo '';
+            echo $n1 + $n2;
         }   else {
             $n1 = $_POST['numero1'];
             $n2 = $_POST['numero2'];
-            echo  $n1 . ' + ' . $n2 . ' = ' . $n1+$n2;
+
+            
         }
     }
 ?>
@@ -24,5 +25,18 @@
         <input type="submit">
     </form>
 </body>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $n1 = $_POST['numero1'];
+    $n2 = $_POST['numero2'];
+    $resultado = $n1+$n2;
+    echo $n1 + $n2 = $resultado;
+
+
+}
+?>
+
+
+
 
 </html>
