2c2e049dc369fb4c069ccfe9144398eb548f04c2
Matheus <99matheussan@gmail.com>
2022-05-27 16:30:34 -0300

    update

--- www/Exercicios-PHP/ex011.php
+++ www/Exercicios-PHP/ex011.php
@@ -1,23 +1,28 @@
 <!-- Solicite os valores de num1 e num2. Some os valores das variaveis num1 e num2: sendo num1=52 e num2 = 106. por fim exiba o resultado. -->
 <html>
+<?php
+    $n1 = 52; $n2 = 106;
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST['numero1']) || empty($_POST['numero2'])) {
+            $n1 = 52;
+            $n2 = 106;
+            echo '';
+        }   else {
+            $n1 = $_POST['numero1'];
+            $n2 = $_POST['numero2'];
+            echo  $n1 . ' + ' . $n2 . ' = ' . $n1+$n2;
+        }
+    }
+?>
 
 <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Numero1: <input type="number" name="fnumero1" value=<?php echo $var=52?>>
-        Numero2: <input type="number" name="fnumero2" value=<?php echo $var =106?>>
+
+    <form method="post"?>">
+        Numero1: <input type="number" name="numero1" value=<?php echo $n1?>>
+        Numero2: <input type="number" name="numero2" value=<?php echo $n2?>>
         <input type="submit">
     </form>
 </body>
 
 </html>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $numero = $_POST['fnumero1'];
-    $numero2 = $_POST['fnumero2'];
-
-    
-    echo 'O primeiro número digitado foi: ' . $numero . '<br> O segundo número foi: ' . $numero2 . '<br> A soma dos dois número é igual a: ' . $numero + $numero2;
-}
-?>
-<?php
-?>
