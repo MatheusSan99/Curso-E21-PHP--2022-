22bdf187e64066c3ce42a6e57e8ec9bd22d91cf1
Matheus <99matheussan@gmail.com>
2022-06-08 14:13:19 -0300

    update ex 28

--- www/Exercicios-PHP/ex028.php
+++ www/Exercicios-PHP/ex028.php
@@ -1,24 +1,23 @@
-<!-- Crie um cadastro de Pessoas com 5 pessoas utilizando Array. Cada pessoa deve ter os seguintes valores cadastrados: Nome, idade, Cep. A Chave primeria da Array será o numero 1,2,3,4,5. -->
+<!-- Crie um cadastro de Pessoas com 5 pessoas utilizando Array. Cada pessoa deve ter os seguintes valores cadastrados: Nome, idade, Cep. A Chave primária da Array será o numero 1,2,3,4,5. -->
 <style>
     <?php include '../css_form.css' ?>
 </style>
 <title>Exercício 28</title>
 <?php
 
-$cadastroDePessoas = [
-    1 => array('Bruno', 'Idade' => 27, 'CEP' => "Bruno CEP"),
-    2 => array('Junior', 'Idade' =>  30, 'CEP' => "Junior CEP"),
-    3 => array('Marli', 'Idade' => 29, 'CEP' => "Marli CEP"),
-    4 => array('Thiago', 'Idade' =>  25, 'CEP' => "Thiago CEP"),
-    5 => array('Willian', 'Idade' =>  18, 'CEP' => "Willian CEP")
+$cadastroPessoas = [
+1 => ['Nome' => 'Marli', 'Idade' => 30, 'CEP' => '123'],
+2 => ['Nome' => 'Willian', 'Idade' => 18, 'CEP' => '234'],
+3 => ['Nome' => 'Cláudio', 'Idade' => 27, 'CEP' => '456'],
+4 => ['Nome' => 'Thiago', 'Idade' => 25, 'CEP' => '789'],
+5 => ['Nome' => 'Bruno', 'Idade' => 26, 'CEP' => '000'],
 ];
 
-    foreach($cadastroDePessoas as $arraysCadastrados) {
-        echo '<br>';
-        foreach($arraysCadastrados as $chaveIdentificadoraDoAtributo => $dadoInformado) {
-            echo $chaveIdentificadoraDoAtributo . ' : ';
-            echo $dadoInformado . '<br>'; 
-           
-        }
-    }
+foreach($cadastroPessoas as $informacoesIndices => $dadosIndice) {
+    ['Nome' => $nome, 'Idade' => $idade, 'CEP' => $cep] = $dadosIndice;
+
+    echo "Nome: {$nome}<br>";
+    echo "Idade: {$idade}<br>";
+    echo "CEP: {$cep}<br>";
+}
 ?>
