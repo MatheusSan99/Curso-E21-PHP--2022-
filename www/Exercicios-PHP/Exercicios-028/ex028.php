922edfd91fcec4ae7e92e7d0478bcc5806066d31
Matheus <99matheussan@gmail.com>
2022-06-10 22:48:37 -0300

    update

--- www/Exercicios-PHP/Exercicios-028/ex028.php
+++ www/Exercicios-PHP/Exercicios-028/ex028.php
@@ -2,24 +2,23 @@
 <style>
     <?php include '../../css_form.css' ?>
 </style>
+
 <title>Exercício 28</title>
 <?php
+  
 $marli = ['Nome' => 'Marli', 'Idade' => 30, 'CEP' => '123'];
 $willian = ['Nome' => 'Willian', 'Idade' => 18, 'CEP' => '234'];
 $claudio = ['Nome' => 'Cláudio', 'Idade' => 27, 'CEP' => '456'];
 $thiago = ['Nome' => 'Thiago', 'Idade' => 25, 'CEP' => '789'];
 $bruno = ['Nome' => 'Bruno', 'Idade' => 26, 'CEP' => '000'];
-$debora = ['Nome' => 'Debora', 'Idade' => 20, 'CEP' => '121'];
 
 $cadastroPessoas = [
     1 => $bruno,
     2 => $claudio,
-    3 => $debora,
-    4 => $marli,
-    5 => $thiago,
-    6=> $willian];
+    3 => $marli,
+    4 => $thiago,
+    5=> $willian];
 
-    $teste = [0 => $cadastroPessoas];
 function exibeCadastro(array $cadastro) 
 {
 foreach($cadastro as $informacoesIndices => $dadosIndice) {
--- www/Exercicios-PHP/Exercicios-028/ex028b.php
+++ www/Exercicios-PHP/Exercicios-028/ex028b.php
@@ -1,19 +1,28 @@
+<style>
+<?php include '../../css_form.css' ?>
+</style>
 <?php
-// Utilizando o exercicio acima, (mantenha os 5 cadastros ja existentes e) faça um formulario permita insersão de novas pessoas.
-require_once 'ex028.php';
+session_start();
+if(!isset($_SESSION["required"])){
+    $_SESSION['required'] = true;
+    require_once 'ex028.php';
+    $_SESSION['cadastro'] = $cadastroPessoas;
+}
+
+
 ?>
 <title>Ex 28b</title>
 
 <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <form method="post">
         <label for="nome">Digite o Nome:</label>
-        <input type="text" name="nome" placeholder="Insira o nome sem acentos"   >
+        <input type="text" name="nome" placeholder="Insira o nome sem acentos">
 
         <label for="idade">Digite a Idade:</label>
-        <input type="number" name="idade" placeholder="1 a 100"   >
+        <input type="number" name="idade" placeholder="1 a 100">
 
         <label for="cep">Digite o CEP:</label>
-        <input type="number" name="cep" placeholder="123.456.789-0"    >
+        <input type="number" name="CEP" placeholder="123.456.789-0">
         <input type="submit">
     </form>
     <?php
@@ -21,11 +30,26 @@ require_once 'ex028.php';
 
         $nome_adicionar = $_POST['nome'];
         $idade_adicionar = $_POST['idade'];
-        $cep_adicionar = $_POST['cep'];
+        $cep_adicionar = $_POST['CEP'];
+       
+        $adiciona_Pessoas = [
+            'Nome' => $nome_adicionar,
+            'Idade' => $idade_adicionar,
+            'CEP' =>  $cep_adicionar
+        ];
+       
+       array_push($_SESSION['cadastro'], $adiciona_Pessoas);
 
-        $adicionaPessoas = ['Nome' => $nome_adicionar, 'Idade' => $idade_adicionar, 'CEP' => $cep_adicionar];
-        
+     foreach($_SESSION['cadastro'] as $indice) {
+        ['Nome' => $nome, 'Idade' => $idade, 'CEP' => $cep] = $indice;
+       
+        echo "Nome: {$nome}<br>";
+        echo "Idade: {$idade}<br>";
+        echo "CEP: {$cep}<br>";
+     }
     }
+   
+
 
     ?>
 </body>
