ed24145467e8287a2f98b3eb247d8f22c19002b4
Matheus <99matheussan@gmail.com>
2022-06-15 14:54:28 -0300

    ex028

--- www/Exercicios-PHP/Exercicios-028/ex028_apaga.php
+++ www/Exercicios-PHP/Exercicios-028/ex028_apaga.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+
 if(!isset($_GET['apagar'])) {
     array_splice($_SESSION['cadastro'] , $_GET['apagar'], 1);
-    header('Location: /ex028b.php');
+
+    header("refresh: 1 ;ex028b.php");
 }
 ?>
