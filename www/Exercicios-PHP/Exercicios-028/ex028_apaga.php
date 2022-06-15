bdde6963262ccb9e16c5f4163db6e6599d8aef11
Matheus <99matheussan@gmail.com>
2022-06-15 14:36:16 -0300

    ex 029

--- www/Exercicios-PHP/Exercicios-028/ex028_apaga.php
+++ www/Exercicios-PHP/Exercicios-028/ex028_apaga.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if($_GET['delete']) {
-    array_splice($_SESSION['cadastro'] , $_GET['delete'], 1);
+if(!isset($_GET['apagar'])) {
+    array_splice($_SESSION['cadastro'] , $_GET['apagar'], 1);
     header('Location: /ex028b.php');
 }
 ?>
--- www/Exercicios-PHP/Exercicios-028/ex028b.php
+++ www/Exercicios-PHP/Exercicios-028/ex028b.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <head>
     <title>Ex 028b</title>
     <style>
@@ -5,7 +7,7 @@
     </style>
 </head>
 <?php
-session_start();
+
 require_once 'ex028.php';
 ?>
 
@@ -21,11 +23,13 @@ require_once 'ex028.php';
             <label for="cep">Digite o CEP:</label>
             <li class="primeira-lista"> <input type="number" name="CEP" placeholder="123.456.789-0"></li>
             <li class="primeira-lista"><input type="submit" value="adicionar" name="adicionar"></li>
-            
+
         </ul>
     </form>
     <?php
-
+    if (isset($_GET['apagar'])) {
+        array_splice($_SESSION['cadastro'], $_GET['apagar'], 1);
+    }
 
     if (isset($_POST['restaura'])) {
         $_SESSION['cadastro'] = $cadastroPessoas;
@@ -33,23 +37,23 @@ require_once 'ex028.php';
 
 
     if (isset($_POST['adicionar'])) {
-        array_push($_SESSION['cadastro'], array("Nome" => $_POST['nome'], "Idade" => $_POST['idade'], "CEP" => $_POST['CEP'])
+        array_push(
+            $_SESSION['cadastro'],
+            array("Nome" => $_POST['nome'], "Idade" => $_POST['idade'], "CEP" => $_POST['CEP'])
         );
     }
 
     foreach ($_SESSION['cadastro'] as $indice) {
         ['Nome' => $nome, 'Idade' => $idade, 'CEP' => $cep] = $indice;
-        echo "<a href='ex028.php'><input type='submit' name='delete' value='Deletar {$nome}'><a/>";
+        echo "<a href='ex028_apaga.php'><input type='submit' name='apagar' value='Apagar Cadastro' {$nome}'><a/>";
         echo "<div class='informacoes-gerais'> Nome: {$nome} </div>";
         echo "<div class='informacoes-gerais'> Idade: {$idade}</div>";
         echo "<div class='informacoes-gerais'> CEP: {$cep}</div>";
-        
+
         echo "</div>";
         echo "<br>";
     }
-    if($_GET['delete']) {
-        array_splice($_SESSION['cadastro'], $_GET['delete'],1);
-    }
+
     ?>
     <form method="post">
         <input type="submit" name="restaura" value="restaura">
