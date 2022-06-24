46f03f62be92d09b65b4b7652670d1e02e0bbed9
Matheus <99matheussan@gmail.com>
2022-06-24 01:05:36 -0300

    Update

--- www/Exercicios-PHP/TestesPHP/FormularioComSQL/tarefas.php
+++ www/Exercicios-PHP/TestesPHP/FormularioComSQL/tarefas.php
@@ -1,7 +1,9 @@
 <?php
+
 ?>
 <html lang="pt-br">
     <head>
+        <link rel="stylesheet" href="tarefas.css">
         <meta charset="UTF-8">
         <title>Gerenciador de Tarefas</title>
     </head>
@@ -10,10 +12,29 @@
         <form>
             <fieldset>
                 <legend>Nova tarefa</legend>
-                <label>
-                    Tarefa:
-                    <input type="text" name="nome" />
-                </label>
+
+                    <label>
+                        Tarefa:
+                        <input type="text" name="nome" />
+                    </label>
+
+                    <label>Descrição (opcional)
+                        <input type="text" name="descrição"></input>
+                    </label>
+
+                    <label>Prazo (opcional):
+                        <input type="text" name="prazo">
+                    </label>
+                <legend>Prioridade:</legend>
+                    <label>
+                        <input type="radio" name="prioridade" value="baixa" checked>Baixa
+                        <input type="radio" name="prioridade" value="media" >Média
+                        <input type="radio" name="prioridade" value="alta" >Alta
+                    </label>
+
+                    <label>
+                        <input type="checkbox" name="concluida" value="sim">Tarefa Concluída:
+                    </label>
                 <input type="submit" value="Cadastrar" />
             </fieldset>
         </form>
@@ -22,13 +43,21 @@
                 <th>Tarefas</th>
             </tr>
 
+            <?php
+                foreach($listaDeTarefas as $tarefa) : ?>
+                    <tr>
+
+                        <td><?php echo $tarefa['nome']; ?></td>
+                        <td><?php echo $tarefa['descricao']; ?></td>
+                        <td><?php echo $tarefa['prazo']; ?></td>
+                        <td><?php echo $tarefa['prioridade']; ?></td>
+                        <td><?php echo $tarefa['concluida']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+
+
+
         </table>
     </body>
 </html>
-<?php
-$listaDeTarefas = [];
-if (isset($_GET['nome'])) {
-    $listaDeTarefas[] = $_GET['nome'];
-
-}
-?>
